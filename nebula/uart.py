55ebeb0f8efe7939361c4bbafd8e54b748fa2269
Travis F. Collins <travis.collins@analog.com>
2020-03-15 11:46:27 -0700

    Add yaml file config for UART
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- nebula/uart.py
+++ nebula/uart.py
@@ -3,54 +3,98 @@ import time
 import subprocess
 import serial
 import yaml
+import logging
+import threading
+
+logging.basicConfig(level=logging.INFO)
 
 
 class uart:
-    def __init__(self, address='/dev/ttyACM0', fmc="fmcomms2"):
-        self.tftpserverip = "192.168.86.220"
+    def __init__(
+        self,
+        address="/dev/ttyACM0",
+        tftpserverip="192.168.86.220",
+        fmc="fmcomms2",
+        baudrate=115200,
+        logfilename="uart.log",
+        bootargs="console=ttyPS0,115200 root=/dev/mmcblk0p2 rw earlycon rootfstype=ext4 rootwait",
+        yamlfilename=None,
+    ):
+        self.tftpserverip = tftpserverip
         self.address = address
         self.fmc = fmc
-        #self.com = serial.Serial(address, 115200, timeout=0.5)
-        self.com = serial.Serial(address, 115200)
+        self.baudrate = baudrate
+        self.com = serial.Serial(address, baudrate, timeout=0.5)
         self.com.reset_input_buffer()
+        self.listen_thread_run = True
+        self.logfilename = logfilename
+        self.thread = None
+        self.print_to_console = True
+        if yamlfilename:
+            self.update_defaults_from_yaml(yamlfilename)
+
+    def update_defaults_from_yaml(self, filename):
+        stream = open(filename, "r")
+        configs = yaml.safe_load(stream)
+        if "uart-config" not in configs:
+            raise Except("uart-config field not in yaml config file")
+        configsList = configs["uart-config"]
+        for config in configsList:
+            for k in config:
+                if not hasattr(self, k):
+                    raise Exception("Unknown field in uart yaml " + k)
+                setattr(self, k, config[k])
 
     def __del__(self):
-        print("Closing")
+        logging.info("Closing UART")
         self.com.close()
 
-    def read_until_done(self):
-        for k in range(30):
-            data = self.com.readline()
-            data = str(data[:-1].decode('ASCII'))
-            print(data)
-            if data.find("done")>-1:
-                print("DONE")
-                return
-            time.sleep(1)
+    def start_log(self):
+        self.listen_thread_run = True
+        logging.info("Launching UART listening thread")
+        self.thread = threading.Thread(target=self.listen, args=())
+        self.thread.start()
+
+    def stop_log(self):
+        self.listen_thread_run = False
+        logging.info("Waiting for UART reading thread")
+        self.thread.join()
+        logging.info("UART reading thread joined")
+
+    def listen(self):
+        file = open(self.logfilename, "w")
+        while self.listen_thread_run:
+            data = self.read_until_stop()
+            for d in data:
+                file.writelines(d + "\n")
+        file.close()
+        logging.info("UART listening thread closing")
 
     def read_until_stop(self):
         buffer = []
         while self.com.in_waiting > 0:
             data = self.com.readline()
             try:
-                data = str(data[:-1].decode('ASCII'))
+                data = str(data[:-1].decode("ASCII"))
             except:
+                logging.warning("Exception occured during data decode")
                 continue
-            print(data)
+            if self.print_to_console:
+                print(data)
             buffer.append(data)
         return buffer
 
     def write_data(self, data):
-        data = data+"\n"
+        data = data + "\n"
         bdata = data.encode()
-        print("-------------------")
-        print(bdata)
-        print("-------------------")
+        logging.info("--------Sending Data-----------")
+        logging.info(bdata)
+        logging.info("-------------------------------")
         self.com.write(bdata)
         time.sleep(4)
 
     def update_fpga(self):
-        cmd = "tftpboot 0x1000000 "+self.tftpserverip+":system_top.bit"
+        cmd = "tftpboot 0x1000000 " + self.tftpserverip + ":system_top.bit"
         self.write_data(cmd)
         self.read_until_done()
 
@@ -59,44 +103,40 @@ class uart:
         self.read_until_stop()
 
     def update_dev_tree(self):
-        cmd = "tftpboot 0x2A00000 "+self.tftpserverip+":devicetree.dtb"
+        cmd = "tftpboot 0x2A00000 " + self.tftpserverip + ":devicetree.dtb"
         self.write_data(cmd)
         self.read_until_done()
 
     def update_kernel(self):
-        cmd = "tftpboot 0x3000000 "+self.tftpserverip+":uImage"
+        cmd = "tftpboot 0x3000000 " + self.tftpserverip + ":uImage"
         self.write_data(cmd)
         self.read_until_done()
 
     def update_boot_args(self):
-        cmd = "setenv bootargs console=ttyPS0,115200 root=/dev/mmcblk0p2 rw earlycon rootfstype=ext4 rootwait"
+        cmd = "setenv bootargs " + self.bootargs
         self.write_data(cmd)
 
     def boot(self):
         cmd = "bootm 0x3000000 - 0x2A00000"
         self.write_data(cmd)
 
-    def read_for_time(self,period):
+    def read_for_time(self, period):
         for k in range(period):
             data = self.read_until_stop()
             time.sleep(1)
-            #print(data)
 
-def setup_uart():
-    # Read yaml
-    stream = open("fpga-debug.yaml", 'r')
-    configs = yaml.safe_load(stream)
-    board = configs.keys()[0]
-    u = uart(address=board['uart'])
 
 if __name__ == "__main__":
-    u = uart()
 
-    #u.update_fpga()
-    #u.update_dev_tree()
-    #u.update_kernel()
-    #u.update_boot_args()
-    #u.boot()
-    u.read_for_time(120)
+    import pathlib
+
+    p = pathlib.Path(__file__).parent.absolute()
+    p = os.path.split(p)
+    p = os.path.join(p[0], "resources", "nebula-zed.yaml")
+
+    u = uart(yamlfilename=p)
+    u.start_log()
+    time.sleep(10)
+    u.stop_log()
 
     u = []
