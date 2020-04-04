f0e2fe43159b97c1f157f380941f256fb0df6551
Travis F. Collins <travis.collins@analog.com>
2020-04-04 14:16:46 -0700

    Add CLI for tests
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

---
+++ tools/nebtest.py
@@ -0,0 +1,17 @@
+#  type: ignore
+import argsparse
+import nebula
+
+parser = argparse.ArgumentParser(description="Nebula test runner cli")
+parser.add_argument(
+    "-c",
+    action="store",
+    dest="configfilename",
+    required=True,
+    help="Configuration filename",
+)
+
+args = parser.parse_args()
+
+m = nebula.manager(configfilename=args.configfilename)
+m.run_test()
