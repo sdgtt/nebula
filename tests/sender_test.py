aecc574afcbac4301b87880938510392358d1fd5
Travis Collins <travis.collins@analog.com>
2020-04-09 20:10:23 -0700

    Update sender_test.py

--- tests/sender_test.py
+++ tests/sender_test.py
@@ -1,14 +1,14 @@
 import socket
 import time
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 23456  # The port used by the server
+# HOST = "127.0.0.1"  # The server's hostname or IP address
+# PORT = 23456  # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    print("Sending")
-    time.sleep(1)
-    s.sendall(b"Hello, world")
-    time.sleep(4)
-    s.sendall(b"Hello, world2")
-    print("Done")
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     s.connect((HOST, PORT))
+#     print("Sending")
+#     time.sleep(1)
+#     s.sendall(b"Hello, world")
+#     time.sleep(4)
+#     s.sendall(b"Hello, world2")
+#     print("Done")
