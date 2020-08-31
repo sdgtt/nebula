64fc99e08d6e2921c7f1ad842325191233bfd6ed
Travis F. Collins <travis.collins@analog.com>
2020-08-30 18:21:03 -0700

    Add full jtag system load through
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- nebula/jtag.py
+++ nebula/jtag.py
@@ -46,3 +46,41 @@ class jtag(utils):
         cmd += "rst -system; "
         cmd += "con"
         self.run_xsdb(cmd)
+
+    def full_boot(self):
+        assert os.path.isfile("system_top.bit")
+        assert os.path.isfile("fsbl.elf")
+        assert os.path.isfile("u-boot.elf")
+        assert os.path.isfile("uImage")
+        assert os.path.isfile("devicetree.dtb")
+
+        cmd = "connect; "
+        cmd += "after 3000; "
+        cmd += "targets 1; "
+        cmd += "rst -system; "
+        cmd += "con"
+        cmd += "after 3000; "
+
+        cmd += "target 2; "
+        cmd += "dow fsbl.elf; "
+        cmd += "con; "
+        cmd += "after 3000; "
+
+        cmd += "dow u-boot.elf; "
+        cmd += "con; "
+        cmd += "after 3000; "
+
+        cmd += "target 1; "
+        cmd += "stop; "
+        cmd += "after 3000; "
+
+        cmd += "fpga -file system_top.bit; "
+        cmd += "dow -data devicetree.dtb 0x2A00000; "
+        cmd += "dow -data uImage 0x3000000; "
+        cmd += "con; "
+        cmd += "after 3000"
+
+        # u-boot takes over from here
+        # Must not overwrite memory locations
+
+        self.run_xsdb(cmd)
