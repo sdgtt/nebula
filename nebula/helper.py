22a450dc792e9aba35852a16ef8198e99b630dcd
Travis F. Collins <travis.collins@analog.com>
2021-06-13 14:47:35 -0700

    Update helper.py

--- nebula/helper.py
+++ nebula/helper.py
@@ -119,9 +119,7 @@ class helper:
                         )
                     else:
                         # Handle serial translation
-                        print("section",section,"field",field)
                         if section == "uart-config" and field == "address":
-                            print("HERE")
                             value = convert_by_id_to_tty(value)
                         print(value)
                     break
