acff1d1f97ff15fdde473ae8e9621b554d7a1239
Trecia Agoylo <trecia.agoylo@analog.com>
2021-11-02 14:23:10 +0800

    tasks: update docs
    Signed-off-by: Trecia Agoylo <trecia.agoylo@analog.com>

--- nebula/tasks.py
+++ nebula/tasks.py
@@ -126,12 +126,12 @@ def download_sdcard(c, release="2019_R1"):
 @task(
     help={
         "board_name": "Board configuration name. Ex: zynq-zc702-adv7511-ad9361-fmcomms2-3",
-        "source": "Boot file download source. Options are: local_fs, http, artifactory, remote.\nDefault: local_fs",
-        "source_root": "Location of source boot files. Dependent on source.\nFor http and artifactory sources this is a IP or domain name (no http://)",
-        "branch": "Name of branches to get related files. It can be from Linux+HDL folders or from the boot partition folder.\nFor Linx+HDL, enter string [<linuxbranch>, <hdlbranch>]. For boot partition, enter [boot_partition, <bootpartitionbranch>]. \nThis is only used for\bhttp and artifactory sources. Default is [boot_partition, master]",
+        "source": "Boot file download source. Options are: local_fs, artifactory, remote.\nDefault: local_fs",
+        "source_root": "Location of source boot files. Dependent on source.\nFor artifactory sources this is the domain name",
+        "branch": "Name of branches to get related files. Default: release",
         "yamlfilename": "Path to yaml config file. Default: /etc/default/nebula",
-        "filetype": "No arguments required. If set Pluto firmware is downloaded from GitHub. Branch name is used as release name.\nDesign name must be pluto or m2k",
-        
+        "filetype": "Selects type of related files to be downloaded. Options: boot (boot_partition files), noos (no-OS files), microblaze (microblaze files), firmware . Defaut: boot",
+        "boot_partition": "If filetype is boot and boot_partition is True, boot files are downloaded from boot partition folder, else from hdl and linux folders. Default: True "
     },
 )
 def download_boot_files(
