2ca32e0bb57928d30588982ca74f688716087fe1
kimpaller <kimpaller>
2022-07-22 16:15:30 +0800

    tasks: add run_command task to run shell command over SSH
    Signed-off-by: kimpaller <kimpaller>

--- nebula/tasks.py
+++ nebula/tasks.py
@@ -1060,11 +1060,40 @@ def run_diagnostics(
     n.run_diagnostics()
 
 
+@task(
+    help={
+        "ip": "IP address of board. Default from yaml",
+        "user": "Board username. Default: root",
+        "password": "Password for board. Default: analog",
+        "board_name": "Name of DUT design (Ex: zynq-zc706-adv7511-fmcdaq2). Require for multi-device config files",
+        "command": "Shell command to run via ssh. Supports linux systems for now.",
+        "ignore_exception": "Ignore errors encountered on the remote side.",
+        "retries": "Number of execution attempts",
+    }
+)
+def run_command(
+    c,
+    ip=None,
+    user="root",
+    password="analog",
+    board_name=None,
+    command=None,
+    ignore_exception=False,
+    retries=3,
+):
+    """Run command to expand sd card via IP"""
+    n = nebula.network(
+        dutip=ip, dutusername=user, dutpassword=password, board_name=board_name
+    )
+    n.run_ssh_command(command, ignore_exception, retries)
+
+
 net = Collection("net")
 net.add_task(restart_board)
 net.add_task(update_boot_files)
 net.add_task(check_dmesg)
 net.add_task(run_diagnostics)
+net.add_task(run_command)
 
 
 #############################################
